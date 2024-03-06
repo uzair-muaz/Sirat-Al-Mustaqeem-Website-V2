acc1f89c5fcb40c39944f6d3014d3a3509f8038f
uzair-muaz <uzair.muaz3k@gmail.com>
2024-03-06 21:09:53 +0500

    Why Us Section Completed

--- src/utils/website/constants/index.js
+++ src/utils/website/constants/index.js
@@ -24,3 +24,46 @@ export const navLinks = [
     label: 'Contact Us',
   },
 ];
+
+export const USPsArray = [
+  {
+    image: '/website-assets/hero-image-2.jpg',
+    height: '350px',
+    uspName: 'Academic Excellence',
+    uspDescription:
+      'Providing a rigorous curriculum to foster academic success',
+  },
+  {
+    image: '/website-assets/hero-image-1.jpg',
+    height: '600px',
+    uspName: 'Extracurricular Activities',
+    uspDescription:
+      'Offering a diverse range of activities for holistic development',
+  },
+  {
+    image: '/website-assets/hero-image-3.jpg',
+    height: '600px',
+    uspName: 'Dedicated Faculty',
+    uspDescription:
+      'Experienced and passionate teachers committed to student growth',
+  },
+  {
+    image: '/website-assets/hero-image-2.jpg',
+    height: '350px',
+    uspName: 'Modern Facilities',
+    uspDescription:
+      'State-of-the-art facilities to enhance the learning environment',
+  },
+  {
+    image: '/website-assets/hero-image-1.jpg',
+    height: '350px',
+    uspName: 'Community Engagement',
+    uspDescription: 'Fostering a sense of community and social responsibility',
+  },
+  {
+    image: '/website-assets/hero-image-3.jpg',
+    height: '600px',
+    uspName: 'Innovative Technology',
+    uspDescription: 'Integrating cutting-edge technology to enhance education',
+  },
+];
